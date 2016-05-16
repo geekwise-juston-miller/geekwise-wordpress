03f07b2f633401d01977dc902535b2b50076bd98
Juston Miller <geekwise.juston.miller@gmail.com>
2016-05-16 19:42:21 +0000

    mid-play

--- wp-content/themes/html5blank-stable/courses.php
+++ wp-content/themes/html5blank-stable/courses.php
@@ -1,8 +1,13 @@
 <?php /* Template Name: courses */ ?>
 
 <?php get_header('courses'); ?>
-      
-      
+
+<h1><?php the_title(); ?></h1>
+<?php while (have_posts() ): the_post();?>
+<p class="course-content"><?php the_content(); ?></p>
+<?php endwhile; ?>
+
+
         <div id="workshops_fluff" class="courses_fluff">
             <p>
             <span></span>
@@ -15,15 +20,16 @@
           </p>
         </div>
         
-<!-- added this nav to make background white, which can be seen when looking at the owl background image on this page -->
         
 <nav id="white_background">
-   
+
 <!-- table title -->
     <section class="courses_table" id="courses_title">
         <div class="column1" id="courses_height">
             <p>
                 <span class="courses_bold">
+                    
+                    Topic
             
                 </span>
             </p>
@@ -32,6 +38,8 @@
             <p>
                 <span class="courses_bold">
                     
+                    Duration
+                    
                 </span>
             </p>
         </div>
@@ -39,12 +47,16 @@
             <p>
                 <span class="courses_bold">
                     
+                    Day/Time
+                    
                 </span>
             </p>
         </div>
         <div class="column4">
             <p>
                 <span class="courses_bold">
+                    
+                    Fee	
                     
                 </span>
             </p>
@@ -53,12 +65,16 @@
             <p>
                 <span class="courses_bold">
                     
+                    Instructor
+                    
                 </span>
             </p>
         </div>
         <div class="column6">
             <p>
                 <span class="courses_bold">
+                    
+                    Sign-up
                     
                 </span>
             </p>
@@ -66,20 +82,25 @@
     </section>
         
 <!-- table 1 -->
-   
+  
+<?php query_posts('post_type=geekwise_courses'); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+
     <section class="courses_table courses_border_top" id="courses_table1">
         <div class="column1">
             <p class="courses_underline" id="courses_subtitle1">
+                    <?php the_title(); ?>
                 <span class="sold_out">&nbsp;</span>
             <p class="courses_padding_bottom1">
                 <i>
+                    <?php the_content(); ?>
                 </i>
             </p>
         </div>
         <div class="column2">
             <p>
                 <span class="courses_bold courses_position_top">
-                    
+                    <?php the_field('duration'); ?>
                 </span>
             </p>
             <p class="courses_font_weight">
@@ -92,7 +113,7 @@
         <div class="column3 courses_column_Monday">
             <p>
                 <span  class="courses_bold">
-                    
+                    <?php the_field('day_time'); ?>
                 </span>
             </p>
             <p  class="courses_padding_bottom2">
@@ -101,7 +122,7 @@
         <div class="column4">
             <p>
                 <span class="courses_bold">
-                    
+                    <?php the_field('fee'); ?>
                 </span>
             </p>
             <p  class="courses_padding_bottom2">
@@ -110,266 +131,270 @@
         </div>
         <div class="column5">
             <p class="courses_padding_bottom3">
-                
+                <?php the_field('instructor'); ?>
             </p>
         </div>
         <div class="column6">
             <p class="courses_enroll">
                 <a href="#" class="enroll_anchor">
                     <span>
-                        
+                        <button class="btn">Enroll</button>
                     </span>
                 </a>
             </p>
         </div>
     </section>
+    
+<?php endwhile; ?>    
         
-<!-- table 2 -->
+        
+        
+ <!--table 2 -->
        
-    <section class="courses_table courses_border_top" id="courses_table2">
-        <div class="column1">
-            <p class="courses_underline" id="courses_subtitle2">
-                <span class="sold_out">&nbsp;</span>
-            </p>
-            <p class="courses_padding_bottom1">
-                <i>
+ <!--   <section class="courses_table courses_border_top" id="courses_table2">-->
+ <!--       <div class="column1">-->
+ <!--           <p class="courses_underline" id="courses_subtitle2">-->
+ <!--               <span class="sold_out">&nbsp;</span>-->
+ <!--           </p>-->
+ <!--           <p class="courses_padding_bottom1">-->
+ <!--               <i>-->
                     
-                </i>
-            </p>
-        </div>
+ <!--               </i>-->
+ <!--           </p>-->
+ <!--       </div>-->
         
-<!-- need to redo these columns because found out when doing media query some were in one p tag instead of separated ones for each line -->
+ <!--need to redo these columns because found out when doing media query some were in one p tag instead of separated ones for each line -->
         
-        <div class="column2">
-            <p>
-                <span class="courses_bold courses_position_top">
+ <!--       <div class="column2">-->
+ <!--           <p>-->
+ <!--               <span class="courses_bold courses_position_top">-->
                     
-                </span>
-            </p>
-            <p>
+ <!--               </span>-->
+ <!--           </p>-->
+ <!--           <p>-->
                
-            </p>
-            <p>
+ <!--           </p>-->
+ <!--           <p>-->
                 
-            </p>
-        </div>
-        <div class="column3 courses_column_Monday">
-            <p>
-                <span  class="courses_bold">
+ <!--           </p>-->
+ <!--       </div>-->
+ <!--       <div class="column3 courses_column_Monday">-->
+ <!--           <p>-->
+ <!--               <span  class="courses_bold">-->
                     
-                </span>
-            </p>
-            <p  class="courses_padding_bottom2">
+ <!--               </span>-->
+ <!--           </p>-->
+ <!--           <p  class="courses_padding_bottom2">-->
                 
-            </p>
-        </div>
-        <div class="column4">
-            <p>
-                <span class="courses_bold">
+ <!--           </p>-->
+ <!--       </div>-->
+ <!--       <div class="column4">-->
+ <!--           <p>-->
+ <!--               <span class="courses_bold">-->
                     
-                </span>
-            </p>
-            <p  class="courses_padding_bottom2">
+ <!--               </span>-->
+ <!--           </p>-->
+ <!--           <p  class="courses_padding_bottom2">-->
                 
-            </p>
-        </div>
-        <div class="column5">
-            <p class="courses_padding_bottom3">
+ <!--           </p>-->
+ <!--       </div>-->
+ <!--       <div class="column5">-->
+ <!--           <p class="courses_padding_bottom3">-->
                 
-            </p>
-        </div>
-        <div class="column6">
-            <p class="courses_enroll">
-                <a href="#" class="enroll_anchor">
-                    <span>
+ <!--           </p>-->
+ <!--       </div>-->
+ <!--       <div class="column6">-->
+ <!--           <p class="courses_enroll">-->
+ <!--               <a href="#" class="enroll_anchor">-->
+ <!--                   <span>-->
                         
-                    </span>
-                </a>
-            </p>
-        </div>
-        </section>
+ <!--                   </span>-->
+ <!--               </a>-->
+ <!--           </p>-->
+ <!--       </div>-->
+ <!--       </section>-->
         
-<!-- table 3 -->
-        <section class="courses_table courses_border_top">
-        <div class="column1">
-            <p class="courses_underline" id="courses_subtitle3">
+ <!--table 3 -->
+        <!--<section class="courses_table courses_border_top">-->
+        <!--<div class="column1">-->
+        <!--    <p class="courses_underline" id="courses_subtitle3">-->
                 
-            </p>
-            <p class="courses_padding_bottom1">
-                <i>
-              </i>
-            </p>
-        </div>
-        <div class="column2">
-            <p>
-                <span class="courses_bold courses_position_top">
+        <!--    </p>-->
+        <!--    <p class="courses_padding_bottom1">-->
+        <!--        <i>-->
+        <!--      </i>-->
+        <!--    </p>-->
+        <!--</div>-->
+        <!--<div class="column2">-->
+        <!--    <p>-->
+        <!--        <span class="courses_bold courses_position_top">-->
                 
-                </span>
-            </p>
-            <p>
+        <!--        </span>-->
+        <!--    </p>-->
+        <!--    <p>-->
               
-            </p>
-            <p>
+        <!--    </p>-->
+        <!--    <p>-->
                 
-            </p>
-        </div>
-        <div class="column3 courses_column_Monday">
-            <p>
-                <span  class="courses_bold">
+        <!--    </p>-->
+        <!--</div>-->
+        <!--<div class="column3 courses_column_Monday">-->
+        <!--    <p>-->
+        <!--        <span  class="courses_bold">-->
                     
-                </span>
-            </p>
-            <p  class="courses_padding_bottom2">
+        <!--        </span>-->
+        <!--    </p>-->
+        <!--    <p  class="courses_padding_bottom2">-->
                 
-            </p>
-        </div>
-        <div class="column4">
-            <p>
-                <span class="courses_bold">
+        <!--    </p>-->
+        <!--</div>-->
+        <!--<div class="column4">-->
+        <!--    <p>-->
+        <!--        <span class="courses_bold">-->
                     
-                </span>
-            </p>
-            <p  class="courses_padding_bottom2">
+        <!--        </span>-->
+        <!--    </p>-->
+        <!--    <p  class="courses_padding_bottom2">-->
                 
-            </p>
-        </div>
-        <div class="column5">
-            <p class="courses_padding_bottom3">
+        <!--    </p>-->
+        <!--</div>-->
+        <!--<div class="column5">-->
+        <!--    <p class="courses_padding_bottom3">-->
                 
-            </p>
-        </div>
-        <div class="column6">
-            <p class="courses_enroll">
-                <a href="#" class="enroll_anchor">
-                    <span>
+        <!--    </p>-->
+        <!--</div>-->
+        <!--<div class="column6">-->
+        <!--    <p class="courses_enroll">-->
+        <!--        <a href="#" class="enroll_anchor">-->
+        <!--            <span>-->
                         
-                    </span>
-                </a>
-            </p>
-        </div>
-        </section>
+        <!--            </span>-->
+        <!--        </a>-->
+        <!--    </p>-->
+        <!--</div>-->
+        <!--</section>-->
         
-<!-- table 4 -->
-        <section class="courses_table courses_border_top">
-        <div class="column1">
-            <p class="courses_underline" id="courses_subtitle4">
+ <!--table 4 -->
+        <!--<section class="courses_table courses_border_top">-->
+        <!--<div class="column1">-->
+        <!--    <p class="courses_underline" id="courses_subtitle4">-->
                 
-            </p>
-            <p class="courses_padding_bottom1">
-                <i>
-               </i>
-            </p>
-        </div>
-        <div class="column2">
-            <p>
-                <span class="courses_bold courses_position_top">
+        <!--    </p>-->
+        <!--    <p class="courses_padding_bottom1">-->
+        <!--        <i>-->
+        <!--       </i>-->
+        <!--    </p>-->
+        <!--</div>-->
+        <!--<div class="column2">-->
+        <!--    <p>-->
+        <!--        <span class="courses_bold courses_position_top">-->
                     
-                </span>
-            </p>
-            <p>
+        <!--        </span>-->
+        <!--    </p>-->
+        <!--    <p>-->
                 
-            </p>
-            <p>
+        <!--    </p>-->
+        <!--    <p>-->
                 
-            </p>
-        </div>
-        <div class="column3 courses_column_Monday">
-            <p>
-                <span  class="courses_bold">
+        <!--    </p>-->
+        <!--</div>-->
+        <!--<div class="column3 courses_column_Monday">-->
+        <!--    <p>-->
+        <!--        <span  class="courses_bold">-->
                     
-                </span>
-            </p>
-            <p class="courses_padding_bottom2">
+        <!--        </span>-->
+        <!--    </p>-->
+        <!--    <p class="courses_padding_bottom2">-->
                 
-            </p>
-        </div>
-        <div class="column4">
-            <p>
-                <span class="courses_bold">
+        <!--    </p>-->
+        <!--</div>-->
+        <!--<div class="column4">-->
+        <!--    <p>-->
+        <!--        <span class="courses_bold">-->
                     
-                </span>
-            </p>
-            <p  class="courses_padding_bottom2">
+        <!--        </span>-->
+        <!--    </p>-->
+        <!--    <p  class="courses_padding_bottom2">-->
                 
-            </p>
-        </div>
-        <div class="column5">
-            <p class="courses_padding_bottom3">
+        <!--    </p>-->
+        <!--</div>-->
+        <!--<div class="column5">-->
+        <!--    <p class="courses_padding_bottom3">-->
                 
-            </p>
-        </div>
-        <div class="column6">
-            <p class="courses_enroll">
-                <a href="#" class="enroll_anchor">
-                    <span>
+        <!--    </p>-->
+        <!--</div>-->
+        <!--<div class="column6">-->
+        <!--    <p class="courses_enroll">-->
+        <!--        <a href="#" class="enroll_anchor">-->
+        <!--            <span>-->
                     
-                    </span>
-                </a>
-            </p>
-        </div>
-        </section>
-<!-- table 5 -->
-        <section class="courses_table courses_border_top courses_border_bottom">
-        <div class="column1">
-            <p class="courses_underline" id="courses_subtitle5">
+        <!--            </span>-->
+        <!--        </a>-->
+        <!--    </p>-->
+        <!--</div>-->
+        <!--</section>-->
+ <!--table 5-->
+       <!-- <section class="courses_table courses_border_top courses_border_bottom">-->
+       <!-- <div class="column1">-->
+       <!--     <p class="courses_underline" id="courses_subtitle5">-->
                 
-            </p>
-            <p class="courses_padding_bottom1">
-                <span>
-                    <i>
+       <!--     </p>-->
+       <!--     <p class="courses_padding_bottom1">-->
+       <!--         <span>-->
+       <!--             <i>-->
                         
-                    </i>
-                </span>
-            </p>
-        </div>
-        <div class="column2">
-            <p>
-                <span class="courses_bold courses_position_top">
+       <!--             </i>-->
+       <!--         </span>-->
+       <!--     </p>-->
+       <!-- </div>-->
+       <!-- <div class="column2">-->
+       <!--     <p>-->
+       <!--         <span class="courses_bold courses_position_top">-->
                     
-                </span>
-            </p>
-            <p>
+       <!--         </span>-->
+       <!--     </p>-->
+       <!--     <p>-->
                 
-            </p>
-            <p>
+       <!--     </p>-->
+       <!--     <p>-->
                 
-            </p>
-        </div>
-        <div class="column3 courses_column_Monday">
-            <p>
-                <span  class="courses_bold">
+       <!--     </p>-->
+       <!-- </div>-->
+       <!-- <div class="column3 courses_column_Monday">-->
+       <!--     <p>-->
+       <!--         <span  class="courses_bold">-->
                     
-                </span>
-            </p>
-            <p class="courses_padding_bottom2">
+       <!--         </span>-->
+       <!--     </p>-->
+       <!--     <p class="courses_padding_bottom2">-->
                 
-            </p>
-        </div>
-        <div class="column4">
-            <p>
-                <span class="courses_bold">
+       <!--     </p>-->
+       <!-- </div>-->
+       <!-- <div class="column4">-->
+       <!--     <p>-->
+       <!--         <span class="courses_bold">-->
                     
-                </span>
-            </p>
-            <p  class="courses_padding_bottom2">
+       <!--         </span>-->
+       <!--     </p>-->
+       <!--     <p  class="courses_padding_bottom2">-->
                 
-            </p>
-        </div>
-        <div class="column5">
-            <p class="courses_padding_bottom3">
+       <!--     </p>-->
+       <!-- </div>-->
+       <!-- <div class="column5">-->
+       <!--     <p class="courses_padding_bottom3">-->
                 
-            </p>
-        </div>
-        <div class="column6">
-            <p class="courses_enroll">
-                <a href="#" class="enroll_anchor">
-                    <span>
+       <!--     </p>-->
+       <!-- </div>-->
+       <!-- <div class="column6">-->
+       <!--     <p class="courses_enroll">-->
+       <!--         <a href="#" class="enroll_anchor">-->
+       <!--             <span>-->
                     
-                    </span>
-                </a>
-            </p>
-        </div>
-       </section>
+       <!--             </span>-->
+       <!--         </a>-->
+       <!--     </p>-->
+       <!-- </div>-->
+       <!--</section>-->
       </nav>
     </main>
     <footer class="workshops_footer">
--- wp-content/themes/html5blank-stable/header-courses.php
+++ wp-content/themes/html5blank-stable/header-courses.php
@@ -16,7 +16,7 @@
     
 </head>
 
-    <body <?php body_class(); ?>>       
+    <body>       
 
        
         <div id="margin_top">
